21f3b28ad86369b6c35c09faddde9503eaab3fd9
Erik Tomlinson <eptomlinson@gmail.com>
2020-09-27 17:55:14 -0500

    added for loop to generate random values for password, attempted to add password text to box, so far unsuccessful

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-// Assignment Code - CREATION OF 1ST VARIABLE
-var generateBtn = document.querySelector("#generate");
-// Add event listener to generate button - GIVE LIFE (FUNCATIONALITY) TO BUTTON IN THE HTML, MUST BE MOVED BEFORE writePASSWORD TO INFORM function
-generateBtn.addEventListener("click", writePassword);
+// // Assignment Code - LINK BUTTON WITH ID FOR BUTTON IN HTML. START BY TELLING COMPUTER THERE IS A BUTTON
+// var generateButton = document.querySelector("#generate");
+// // Add event listener to generate button - GIVE FUNCATIONALITY TO BUTTON IN THE HTML, MUST BE MOVED BEFORE writePASSWORD TO INFORM function
+// generateBtn.addEventListener("click", writePassword)
+// // Write password to the #password input - CREATION OF VARIABLES WITHIN THE FUNCTION OF 1ST VARIABLE
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-// Write password to the #password input - CREATION OF VARIABLES WITHIN THE FUNCTION OF 1ST VARIABLE
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// }
+// // ACTIVATE generatePASSWORD FUNCTION BY GIVING IT VARS OF ITS OWN
+// function generatePassword(){
+// // LIST VARIABLES TO BE TRACKED BY BUTTON, USER INPUTS DATA BASED ON QUESTIONS:
+var length = prompt("How many characters should be in this password?")
+var numbers = confirm("Should this password include numbers?")
+var upperCase = confirm("Should this password include upper case characters?")
+var lowerCase = confirm("Should this password include lower case characters?")
+var specialCharacters = confirm("Should this password include special characters, such as $, %, @, ect.?")
 
-  passwordText.value = password;
+generatebtn.addEventListener("click", writePassword)
 
+// https://www.youtube.com/watch?v=9sT03jEwcaw  SOURCE
+
+// GEN PASSWORD 
+function generate(){
+
+  // SET PASSWORD PARAMETERS
+  var generateButton = document.querySelector("#generate")
+
+  const values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+";
+
+  let password = "";
+
+  // CREATE FOR LOOP TO GEN CHARACTERS
+
+  for(var i = 0; i <= complexity; i++){
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
+
+  // ADD PASSWORD TO TEXT BOX 
+
+  document.getElementById("#generate").values = password;
 }
 
-
-// ACTIVATE generatePASSWORD FUNCTION
-function generatePassword(){
-// LIST VARIABLES TO BE TRACKED BY BUTTON, USER INPUTS DATA BASED ON QUESTIONS:
-  var passwordLength = prompt("How many characters should be in this password?")
-  var numbers = confirm("Should this password include numbers?")
-  var upperCase = confirm("Should this password include upper case characters?")
-  var lowerCase = confirm("Should this password include lower case characters?")
-  var specialCharacters = confirm("Should special characters be included in this password?")
-
-}
-// HOW MANY OF EACH CHARACTER IS REQUIRED? TYPICALLY, WE WILL ALLOW WHATEVER CONFIGURATION OF PASSWORD THE USER DESIRES:
-var minimumCount = 0;
-
-var minimumNumber = "";
-var minimumLowerCase = "";
-var minimumUpperCase = "";
-var minimum SpecialCharacters = "";
-// CHECK TO SEE IF VARS NEED TO BE STRINGS HERE ^^
-
-
-// ONCE PARAMETERES (DATA) HAVE BEEN ENTERED, PASSWORD NEEDS TO BE generatePassword. START BY SETTING UP NEW FUNCTION TO RANDOMIZE DATA. 
